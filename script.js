93d2d24248e6fc24aa9a5c732425a47a74631c7d
faizann@hp <fdar5593@gmail.com>
2026-03-23 15:47:42 +0530

    add files

---
+++ script.js
@@ -0,0 +1,13 @@
+let a = document.getElementById("f3");
+let b = document.getElementById("k1");
+let c = document.getElementById("k2");
+function l1() {
+  a.style.display = "block";
+  b.style.display = "none";
+  c.style.display = "block";
+}
+function l2() {
+  a.style.display = "none";
+  b.style.display = "block";
+  c.style.display = "none";
+}
